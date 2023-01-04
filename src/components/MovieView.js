66ccc046684e0fa9487b513378c6b5ea4a4058ae
Olmen Vicente <sethvicente1@gmail.com>
2023-01-04 15:40:16 -0800

    added small details to the movie view

--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -48,23 +48,38 @@ export const MovieView = () => {
           );
         }
       }
+
       return (
         <>
           <Hero text={movieDetails.original_title} backdrop={backdropUrl} />
-          <div className="container my-5">
-            <div className="row">
-              <div className="col-md-3">
-                {posterLoaded(
-                  movieDetails.poster_path,
-                  movieDetails.original_title
-                )}
-              </div>
-              <div className="col-md-9">
-                <h2>{movieDetails.original_title}</h2>
-                <p className="fs-4">Rating: {movieDetails.vote_average}/10</p>
-                <p className="fs-3">Run Time: {movieDetails.runtime} minutes</p>
-                <p className="fs2">Status: {movieDetails.status}</p>
-                <p className="lead">{movieDetails.overview}</p>
+          <div
+            style={{
+              background: `linear-gradient(to right, rgba(0,0,0,.8),rgba(0,0,0,.7)),url(${backdropUrl})`,
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+              padding: "20px",
+            }}
+          >
+            <div className="container my-4">
+              <div className="row">
+                <div className="col-md-3">
+                  {posterLoaded(
+                    movieDetails.poster_path,
+                    movieDetails.original_title
+                  )}
+                </div>
+                <div className="col-md-9 text-white">
+                  <h2>{movieDetails.original_title}</h2>
+                  <p className="fs-4">Rating: {movieDetails.vote_average}/10</p>
+                  <p className="fs-3">
+                    Run Time: {movieDetails.runtime} minutes
+                  </p>
+                  <p className="fs2">Status: {movieDetails.status}</p>
+                  <p className="fs2">
+                    Release Date: {movieDetails.release_date}
+                  </p>
+                  <p className="lead">{movieDetails.overview}</p>
+                </div>
               </div>
             </div>
           </div>
