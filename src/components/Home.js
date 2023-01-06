c6c18c6b7a0ad4262f6148828edba4e3d449d37d
Olmen Vicente <sethvicente1@gmail.com>
2023-01-05 21:16:57 -0800

    final test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,15 @@
 import { Hero } from "./Hero";
-// import { About } from "./homeComponents/About";
-// import { News } from "./homeComponents/News";
+import { About } from "./homeComponents/About";
+import { News } from "./homeComponents/News";
 
 const Home = () => {
-  // const homeHero =
-  //   "https://images.unsplash.com/photo-1460881680858-30d872d5b530?";
+  const homeHero =
+    "https://images.unsplash.com/photo-1460881680858-30d872d5b530?";
   return (
     <>
-      <Hero
-        text="Welcome to Movie Browser"
-        // backdrop={homeHero}
-      />
-      {/* <About /> */}
-      {/* <News /> */}
+      <Hero text="Welcome to Movie Browser" backdrop={homeHero} />
+      <About />
+      <News />
     </>
   );
 };
--- src/components/homeComponents/About.js
+++ src/components/homeComponents/About.js
@@ -1,16 +1,16 @@
-// import "./About.css";
+import "./About.css";
 
-// export const About = () => {
-//   return (
-//     <>
-//       <section className="about-section">
-//         <h2 className="subtitle">
-//           Look up Movies details for nearly every movie imaginable
-//         </h2>
-//         <p className="description">
-//           Try the search bar to begin searching for a movie
-//         </p>
-//       </section>
-//     </>
-//   );
-// };
+export const About = () => {
+  return (
+    <>
+      <section className="about-section">
+        <h2 className="subtitle">
+          Look up Movies details for nearly every movie imaginable
+        </h2>
+        <p className="description">
+          Try the search bar to begin searching for a movie
+        </p>
+      </section>
+    </>
+  );
+};
